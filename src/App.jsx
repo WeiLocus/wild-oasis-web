d65bdf254f524414412958f22a9b2f05db126225
WeiLocus <marsuo52@gmail.com>
2023-06-24 13:57:19 +0800

    feat: add as prop to define individual html elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { GlobalStyle } from "./styles/GlobalStyles"
-
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
+import Input from "./ui/Input"
 
 function App() {
 
   return (
     <>
       <GlobalStyle />
-      <div>The Wild Oasis</div>
+      <Heading as="h1">The Wild Oasis</Heading>
+      <Heading as="h2">Check in and out</Heading>
+      <Button >check in</Button>
+      <Button>check out</Button>
+      <Heading as="h3">Form</Heading>
+      <Input type="number" placeholder="number of guest"></Input>
     </>
   );
 }
