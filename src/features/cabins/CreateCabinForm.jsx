cf706e2f140125e9a7ddbc262f2ce602d8c0d4b6
WeiLocus <marsuo52@gmail.com>
2023-06-26 00:26:02 +0800

    feat: add useForm hook to get input data

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -3,6 +3,7 @@ import Form from "../../ui/Form"
 import Input from "../../ui/Input";
 import Button from "../../ui/Button"
 import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
 
 const FormRow = styled.div`
   display: grid;
@@ -35,27 +36,44 @@ const Label = styled.label`
 `;
 
 function CreateCabinForm() {
+
+  const { register, handleSubmit } = useForm()
+
+  function onSubmit(data) {
+    console.log(data)
+
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+        <Input type="text" id="name" {...register("name")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
+        <Input type="number" id="maxCapacity" {...register("maxCapacity")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
+        <Input type="number" id="regularPrice" {...register("regularPrice")} />
       </FormRow>
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register("discount")}
+        />
       </FormRow>
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea id="description" defaultValue="" />
+        <Textarea
+          id="description"
+          defaultValue=""
+          {...register("description")}
+        />
       </FormRow>
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
