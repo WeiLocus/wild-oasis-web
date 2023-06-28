bc9a66b0e4b5fe2c70be1c6e2b51402b9fd65a52
WeiLocus <marsuo52@gmail.com>
2023-06-29 00:44:44 +0800

    delete unnecessary AddCabin component content

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -12,31 +12,8 @@ function AddCabin() {
       <Modal.Window name="cabin-form">
         <CreateCabinForm />
       </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show Table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
     </Modal>
   );
 }
-
-// function AddCabin() {
-//     const [isOpenModal, setIsOpenModal] = useState(false);
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
 
 export default AddCabin;
