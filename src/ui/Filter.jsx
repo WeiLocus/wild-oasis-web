c566407101d059a545dfb6dbbf13efc9994a8278
WeiLocus <marsuo52@gmail.com>
2023-07-01 10:41:38 +0800

    add sort options as props to render in the Select component

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,7 +37,7 @@ function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const currentFilter = searchParams.get(filterField) || options[0].value
-  console.log(currentFilter)
+  // console.log(currentFilter)
 
   // set value into the URL
   function handleClick(value) {
@@ -51,6 +51,7 @@ function Filter({ filterField, options }) {
           key={option.value}
           onClick={() => handleClick(option.value)}
           active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
