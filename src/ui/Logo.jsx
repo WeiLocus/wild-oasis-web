74b09ee07983a6f3d3a2aca376a5fbe5cc518dfd
WeiLocus <marsuo52@gmail.com>
2023-07-03 01:09:52 +0800

    adjust logo image path

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,20 +1,19 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const StyledLogo = styled.div`
   text-align: center;
-`
+`;
 
 const Img = styled.img`
   width: auto;
   height: 9.6rem;
-
-`
+`;
 function Logo() {
   return (
     <StyledLogo>
-      <Img src="/logo-light.png" alt="Logo" />
+      <Img src="./logo-light.png" alt="Logo" />
     </StyledLogo>
   );
 }
 
-export default Logo
+export default Logo;
