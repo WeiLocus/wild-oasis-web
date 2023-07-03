5a847fe6aa55b6ad625bb8c50a1fbe1261be3b40
WeiLocus <marsuo52@gmail.com>
2023-07-04 00:14:33 +0800

    feat: add getBookingDetail api function and add bookingDetail content

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,9 +1,27 @@
+import styled from "styled-components";
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import Tag from "../../ui/Tag";
+
+const HeadingGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+`
+
 function BookingDetail() {
   return (
-    <div>
+    <>
+      <Row type="horizontal">
+        <HeadingGroup>
+          <Heading as="h1">Booking #bookingId</Heading>
+          <Tag>status</Tag>
+        </HeadingGroup>
+        <button>&larr; Back</button>
+      </Row>
       Booking Detail
-    </div>
-  )
+    </>
+  );
 }
 
-export default BookingDetail
+export default BookingDetail;
