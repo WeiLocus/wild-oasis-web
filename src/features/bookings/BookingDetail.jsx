854541477d347a43822fcc89541d54c4033e24bc
WeiLocus <marsuo52@gmail.com>
2023-07-04 00:35:32 +0800

    feat: display bookingId and status in Heading

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
 import Tag from "../../ui/Tag";
+import { useBooking } from "./useBooking";
+import Spinner from "../../ui/Spinner";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -10,12 +12,23 @@ const HeadingGroup = styled.div`
 `
 
 function BookingDetail() {
+  const { isLoading, booking: bookingData } = useBooking();
+  if(isLoading) return <Spinner />
+
+  const { id: bookingId, status } = bookingData;
+
+  const statusToTagName = {
+    unconfirmed: "blue",
+    "checked-in": "green",
+    "checked-out": "silver",
+  };
+
   return (
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #bookingId</Heading>
-          <Tag>status</Tag>
+          <Heading as="h1">Booking #{bookingId}</Heading>
+          <Tag type={statusToTagName[status]}>{status.replace("-"," ")}</Tag>
         </HeadingGroup>
         <button>&larr; Back</button>
       </Row>
