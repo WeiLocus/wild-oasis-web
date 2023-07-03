64aa767addab1d28d59d9a5bf9da3c397078d591
WeiLocus <marsuo52@gmail.com>
2023-07-04 00:58:05 +0800

    destructuring bookingData

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -1,9 +1,40 @@
-function BookingDataBox({bookingData}) {
+import { HiOutlineHomeModern } from "react-icons/hi2";
+import styled from "styled-components";
+
+const StyledBox = styled.section`
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+`;
+
+const Header = styled.header``;
+
+function BookingDataBox({ bookingData }) {
+  const {
+    created_at,
+    startDate,
+    endDate,
+    numNights,
+    numGuests,
+    cabinPrice,
+    extrasPrice,
+    totalPrice,
+    status,
+    hasBreakfast,
+    isPaid,
+    cabins: { name: cabinName },
+    guests: { fillName: guestName, email, country, nationalID, countryFlag },
+  } = bookingData;
   return (
-    <div>
-      
-    </div>
-  )
+    <StyledBox>
+      <Header>
+        <div>
+          <HiOutlineHomeModern />
+          <p>{numNights} night in Cabin <span>{cabinName}</span></p>
+        </div>
+      </Header>
+    </StyledBox>
+  );
 }
 
-export default BookingDataBox
+export default BookingDataBox;
