72991bdf273e81d834ac8bd6a1b56b4e543c0107
WeiLocus <marsuo52@gmail.com>
2023-07-04 00:44:57 +0800

    feat: add useMoveBack function to navigate previous page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,8 +1,9 @@
+import { useBooking } from "./useBooking";
+import { useMoveBack } from "../../hooks/useMoveBack";
 import styled from "styled-components";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
 import Tag from "../../ui/Tag";
-import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import ButtonText from "../../ui/ButtonText";
 
@@ -10,11 +11,12 @@ const HeadingGroup = styled.div`
   display: flex;
   align-items: center;
   gap: 2.4rem;
-`
+`;
 
 function BookingDetail() {
   const { isLoading, booking: bookingData } = useBooking();
-  if(isLoading) return <Spinner />
+  const moveBack = useMoveBack();
+  if (isLoading) return <Spinner />;
 
   const { id: bookingId, status } = bookingData;
 
@@ -29,9 +31,9 @@ function BookingDetail() {
       <Row type="horizontal">
         <HeadingGroup>
           <Heading as="h1">Booking #{bookingId}</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-"," ")}</Tag>
+          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
-        <ButtonText>&larr; Back</ButtonText>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
       Booking Detail
     </>
