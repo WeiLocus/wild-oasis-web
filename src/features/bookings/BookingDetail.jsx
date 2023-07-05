98c6675a82f983ad095e03820c32bffb35507451
WeiLocus <marsuo52@gmail.com>
2023-07-05 17:17:06 +0800

    feat: user can delete booking on the booking details page and go back to bookings page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -2,6 +2,7 @@ import { useBooking } from "./useBooking";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 import styled from "styled-components";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
@@ -11,6 +12,8 @@ import ButtonText from "../../ui/ButtonText";
 import BookingDataBox from "./BookingDataBox";
 import Button from "../../ui/Button";
 import ButtonGroup from "../../ui/ButtonGroup";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,6 +25,7 @@ function BookingDetail() {
   const navigate = useNavigate();
   const { isLoading, booking: bookingData } = useBooking();
   const { isCheckout, checkoutConfirm } = useCheckout();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
   const moveBack = useMoveBack();
   if (isLoading) return <Spinner />;
 
@@ -51,7 +55,24 @@ function BookingDetail() {
             Check in
           </Button>
         )}
-        {status === "checked-in" && <Button onClick={() => checkoutConfirm(bookingId)}>Check out</Button>}
+        {status === "checked-in" && (
+          <Button onClick={() => checkoutConfirm(bookingId)}>Check out</Button>
+        )}
+
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete Booking</Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              onConfirm={() => {
+                deleteBooking(bookingId, {onSettled: () => navigate(-1)})}
+              }
+              disabled={isDeleting}
+            />
+          </Modal.Window>
+        </Modal>
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
