a350392e467d9dedafb752e9f18d1e4de31bae6e
WeiLocus <marsuo52@gmail.com>
2023-07-06 23:23:49 +0800

    feat: add get current user api function to check if user had authentucated

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,14 +1,28 @@
 import supabase from "./supabase";
 
 export async function login({ email, password }) {
-  const  { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
 
-  if(error) {
-    throw new Error(error.message)
+  if (error) {
+    throw new Error(error.message);
   }
   // console.log(data)
-  return data
+  return data;
+}
+
+
+export async function getCurrentUser() {
+  // get data from localStorage
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+  // fetch current user from supabase
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  // console.log("data",data)
+
+  return data?.user;
 }
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useUser } from "../features/authentication/useUser"
+import styled from "styled-components"
+import Spinner from "./Spinner"
+
+const FullPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: var(--color-grey-50);
+`
+
 function ProtectedRoute({children}) {
-  return children
+  const navigate = useNavigate()
+
+  // Load the auth user
+  const { isLoading, isAuthenticated} = useUser()
+
+  // if no auth user redirect to the login page
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  // while loading show spinner
+  if (isLoading) return (
+    <FullPage>
+      <Spinner />
+    </FullPage>
+  );
+  // render app
+  if (isAuthenticated) return children
 }
 
 export default ProtectedRoute
