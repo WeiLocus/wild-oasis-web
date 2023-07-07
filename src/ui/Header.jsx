327cd85dfe3d2d75aec26bd4d114be004ebfcb68
WeiLocus <marsuo52@gmail.com>
2023-07-07 16:02:16 +0800

    feat: add header menu to render user icon and logout icon

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
 
 const StyledHeader = styled.header`
   padding: 1.2rem 4.8rem;
@@ -10,7 +10,7 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
