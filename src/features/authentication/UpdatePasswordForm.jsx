c8c57c4d37a962e7a7473d065b8e75c406245711
WeiLocus <marsuo52@gmail.com>
2023-07-07 23:23:26 +0800

    feat: use react hook form to validation input field

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -1,26 +1,50 @@
+import { useForm } from "react-hook-form";
+import { useUpdateUser } from "./useUpdateUser";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Button from "../../ui/Button";
 
 function UpdatePasswordForm() {
+  const { register, formState, getValues, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  const { isUpdating, updateUser } = useUpdateUser();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="New password (min 8 chars)">
-        <Input type="password" id="password" autoComplete="current-password" />
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password needs a minimum 8 characters",
+            },
+          })}
+        />
       </FormRow>
       <FormRow label="Confirm password">
         <Input
           type="password"
           id="passwordConfirm"
           autoComplete="new-password"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+            validate: (value) =>
+              value === getValues().password || "Password need to match",
+          })}
         />
       </FormRow>
       <FormRow>
-        <Button
-          variation="secondary"
-          type="reset"
-        >
+        <Button variation="secondary" type="reset">
           Cancel
         </Button>
         <Button>Update password</Button>
