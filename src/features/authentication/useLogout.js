006cb291356ea763c964a110a0c6bdb9a3fca898
WeiLocus <marsuo52@gmail.com>
2023-07-07 10:46:05 +0800

    feat: remove all cache from react query when logout web

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,13 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 
 export function useLogout() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient()
 
   const { isLoading, mutate: logout } = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
+      // remove all cache from react query
+      queryClient.removeQueries()
       // 導航至login頁面後不保留所有頁面紀錄
       navigate("login", { replace: true });
     },
