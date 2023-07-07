468064f7ea9e5344328ec6a168add9becf955104
WeiLocus <marsuo52@gmail.com>
2023-07-07 20:38:43 +0800

    feat: get the email and fullName of the current user and add state to manage fullName and avatar

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,27 +1,45 @@
-import Form from "../../ui/Form"
-import FormRow from "../../ui/FormRow"
-import Input from "../../ui/Input"
-import Button from "../../ui/Button"
+import { useUser } from "./useUser";
+import { useState } from "react";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 
 function UpdateUserDataForm() {
+  const { isLoading, user } = useUser();
+  const {
+    email,
+    user_metadata: { fullName: currentFullName },
+  } = user;
+
+  const [fullName, setFullName] = useState(currentFullName);
+  const [avatar, setAvatar] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!fullName) return
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormRow label="Email">
-        <Input />
+        <Input disabled value={email} />
       </FormRow>
       <FormRow label="Full name">
-        <Input />
+        <Input type="text" id="fullName" value={currentFullName} />
       </FormRow>
       <FormRow label="Avatar image">
-        <FileInput />
+        <FileInput id="avatar" accept="image/*" />
       </FormRow>
       <FormRow>
-        <Button variation="secondary" type="reset">Cancel</Button>
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
         <Button>Update account</Button>
       </FormRow>
     </Form>
   );
 }
 
-export default UpdateUserDataForm
+export default UpdateUserDataForm;
