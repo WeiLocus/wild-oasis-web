21e9a225d46c15ba36075d5943984716344ff1a4
WeiLocus <marsuo52@gmail.com>
2023-07-07 16:18:12 +0800

    feat: add user avatar style

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 import { useUser } from "./useUser";
 
+const StyledUserAvatar = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+  color: var(--color-grey-600);
+  font-weight: 500;
+  font-size: 1.4rem;
+`
+
+const Avatar = styled.img`
+  display: block;
+  width: 4rem;
+  height: 3.6rem;
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+  outline: 2px solid var(--color-grey-100);
+`
+
 function UserAvatar() {
   const { user } = useUser();
   const { fullName, avatar} = user.user_metadata
 
-  return <div>
-    <img src={avatar || "default-user.jpg"} alt={`Avatar og ${fullName}`}/>
+  return <StyledUserAvatar>
+    <Avatar src={avatar || "default-user.jpg"} alt={`Avatar og ${fullName}`}/>
     <span>{fullName}</span>
-  </div>;
+  </StyledUserAvatar>;
 }
 
 export default UserAvatar;
