91e7c51fa99b277e52c33352da11d5b9cc2f7c26
WeiLocus <marsuo52@gmail.com>
2023-07-07 20:26:06 +0800

    feat: add data form content

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,9 +1,27 @@
+import Form from "../../ui/Form"
+import FormRow from "../../ui/FormRow"
+import Input from "../../ui/Input"
+import Button from "../../ui/Button"
+import FileInput from "../../ui/FileInput";
+
 function UpdateUserDataForm() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Form>
+      <FormRow label="Email">
+        <Input />
+      </FormRow>
+      <FormRow label="Full name">
+        <Input />
+      </FormRow>
+      <FormRow label="Avatar image">
+        <FileInput />
+      </FormRow>
+      <FormRow>
+        <Button variation="secondary" type="reset">Cancel</Button>
+        <Button>Update account</Button>
+      </FormRow>
+    </Form>
+  );
 }
 
 export default UpdateUserDataForm
