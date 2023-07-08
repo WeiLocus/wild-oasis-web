1b990434c0900e25a11a3c27f2baacdce546a5e6
WeiLocus <marsuo52@gmail.com>
2023-07-08 19:42:36 +0800

    feat: user can lick sun/moon icon to switch mode

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,14 +1,30 @@
 import { createContext, useContext } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import { useEffect } from "react";
 
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  // 根據使用者的系統定義light/dark mode
+  const systemMode = window.matchMedia("(prefers-color-scheme:dark)").matches;
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    systemMode,
+    "isDarkMode"
+  );
   console.log("isDarkMode", isDarkMode);
 
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
   function toggleDarkMode() {
-    setIsDarkMode((isDark) => !isDark)
+    setIsDarkMode((isDark) => !isDark);
   }
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
@@ -22,7 +38,7 @@ function useDarkMode() {
   if (context === undefined) {
     throw new Error("DarkModeContext was used outside of DarkModeProvider");
   }
-  return context
+  return context;
 }
 
 export { DarkModeProvider, useDarkMode };
