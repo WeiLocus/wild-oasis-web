0426c350d210712175cd4371b9f0676f9aa54be9
WeiLocus <marsuo52@gmail.com>
2023-07-08 18:53:21 +0800

    feat: add toggle dark mode function as provider value and add useDarkMode function

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,15 +1,27 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  console.log("isDarkMode", isDarkMode);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark)
+  }
   return (
-    <DarkModeContext.Provider value={{ isDarkMode }}>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
 }
 
-export default DarkModeContext;
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined) {
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+  }
+}
+
+export { DarkModeProvider, useDarkMode };
