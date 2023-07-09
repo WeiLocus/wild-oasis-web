33e39bc14c50f2a511b1c0efdfe44df3c1d9586d
WeiLocus <marsuo52@gmail.com>
2023-07-09 15:37:25 +0800

    feat: get the filtered bookings status data from the useRecentStays function

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,17 +1,20 @@
-import styled from "styled-components"
-import { useRecentBookings } from "./useRecentBookings"
-import Spinner from "../../ui/Spinner"
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStatys";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboard = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
-`
+`;
 
 function DashboardLayout() {
-  const { isLoadingDate, bookings } = useRecentBookings()
-  if (isLoadingDate) return <Spinner />
+  const { isLoadingDate, bookings } = useRecentBookings();
+  const { isLoadingStays, stays, confirmedStays } = useRecentStays();
+
+  if (isLoadingDate || isLoadingStays) return <Spinner />;
   console.log(bookings);
 
   return (
@@ -21,7 +24,7 @@ function DashboardLayout() {
       <div>Chart stay durations</div>
       <div>Chart sales</div>
     </StyledDashboard>
-  )
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
