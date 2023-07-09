3980a63b350317c5d41b234e3b2904996f6cf98a
WeiLocus <marsuo52@gmail.com>
2023-07-09 23:25:59 +0800

    feat: organize data to include booking data range,totalSales,extrasSales to render in the chart

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -8,7 +8,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import {eachDayOfInterval, subDays} from "date-fns"
+import { eachDayOfInterval, subDays, format, isSameDay } from "date-fns";
 import Heading from "../../ui/Heading";
 import { useDarkMode } from "../../context/DarkModeContext";
 
@@ -30,7 +30,7 @@ const StyledSalesChart = styled(DashboardBox)`
   }
 `;
 
-function SalesChart({ bookings, numDays}) {
+function SalesChart({ bookings, numDays }) {
   const { isDarkMode } = useDarkMode();
   const colors = isDarkMode
     ? {
@@ -44,19 +44,37 @@ function SalesChart({ bookings, numDays}) {
         extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
         text: "#374151",
         background: "#fff",
-      }; 
-  
+      };
+
+  // 計算要呈現的日期區間
   const allDates = eachDayOfInterval({
     start: subDays(new Date(), numDays - 1),
-    end: new Date()
-  })
-  console.log(allDates)
+    end: new Date(),
+  });
+
+  const dataList = allDates.map((data) => {
+    // 計算 totalSales: 篩選符合日期區間的資料再做加總
+    const totalSales = bookings
+      .filter((booking) => isSameDay(data, new Date(booking.created_at)))
+      .reduce((acc, current) => acc + current.totalPrice, 0);
+
+    // 計算 extrasSales: 篩選符合日期區間的資料再做加總
+    const extrasSales = bookings
+      .filter((booking) => isSameDay(data, new Date(booking.created_at)))
+      .reduce((acc, current) => acc + current.extrasPrice, 0);
+
+    return {
+      label: format(data, "MMM dd"),
+      totalSales: totalSales,
+      extrasSales: extrasSales
+    };
+  });
 
   return (
     <StyledSalesChart>
       <Heading as="h2">Sales</Heading>
       <ResponsiveContainer height={300} width="100%">
-        <AreaChart data={OLDdata}>
+        <AreaChart data={dataList}>
           <XAxis
             dataKey="label"
             tick={{ fill: colors.text }}
@@ -94,34 +112,3 @@ function SalesChart({ bookings, numDays}) {
 }
 
 export default SalesChart;
-
-const OLDdata = [
-  { label: "Jun 09", totalSales: 480, extrasSales: 320 - 300 },
-  { label: "Jun 10", totalSales: 580, extrasSales: 400 - 300 },
-  { label: "Jun 11", totalSales: 550, extrasSales: 450 - 300 },
-  { label: "Jun 12", totalSales: 600, extrasSales: 350 - 300 },
-  { label: "Jun 13", totalSales: 700, extrasSales: 550 - 300 },
-  { label: "Jun 14", totalSales: 800, extrasSales: 650 - 500 },
-  { label: "Jun 15", totalSales: 700, extrasSales: 700 - 500 },
-  { label: "Jun 16", totalSales: 650, extrasSales: 500 - 300 },
-  { label: "Jun 17", totalSales: 600, extrasSales: 600 - 300 },
-  { label: "Jun 18", totalSales: 550, extrasSales: 400 - 300 },
-  { label: "Jun 19", totalSales: 700, extrasSales: 600 - 500 },
-  { label: "Jun 20", totalSales: 800, extrasSales: 700 - 500 },
-  { label: "Jun 21", totalSales: 700, extrasSales: 600 - 500 },
-  { label: "Jun 22", totalSales: 810, extrasSales: 550 - 500 },
-  { label: "Jun 23", totalSales: 950, extrasSales: 750 - 500 },
-  { label: "Jun 24", totalSales: 970, extrasSales: 600 - 500 },
-  { label: "Jun 25", totalSales: 900, extrasSales: 700 - 500 },
-  { label: "Jun 26", totalSales: 950, extrasSales: 800 - 500 },
-  { label: "Jun 27", totalSales: 850, extrasSales: 700 - 500 },
-  { label: "Jun 28", totalSales: 900, extrasSales: 600 - 500 },
-  { label: "Jun 29", totalSales: 800, extrasSales: 800 - 500 },
-  { label: "Jun 30", totalSales: 950, extrasSales: 700 - 500 },
-  { label: "Jul 01", totalSales: 1200, extrasSales: 900 - 500 },
-  { label: "Jul 02", totalSales: 1250, extrasSales: 800 - 500 },
-  { label: "Jul 03", totalSales: 1400, extrasSales: 950 - 500 },
-  { label: "Jul 04", totalSales: 1500, extrasSales: 1000 - 500 },
-  { label: "Jul 05", totalSales: 1400, extrasSales: 1100 - 500 },
-  { label: "Jul 06", totalSales: 1450, extrasSales: 900 - 500 },
-];
