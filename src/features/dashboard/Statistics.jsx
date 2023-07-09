28d9a85d0dc08a454fd1dc75ac8cab738a1b0512
WeiLocus <marsuo52@gmail.com>
2023-07-09 16:56:31 +0800

    feat: sum totalPrice

--- src/features/dashboard/Statistics.jsx
+++ src/features/dashboard/Statistics.jsx
@@ -1,17 +1,34 @@
-import { HiOutlineBanknotes, HiOutlineBriefcase, HiOutlineCalendarDays, HiOutlineChartBar } from "react-icons/hi2"
-import Stat from "./Stat"
+import { formatCurrency } from "../../utils/helper";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat";
 
 function Statistics({ bookings, confirmedStays }) {
   // stat1
-  const numBookings = bookings.length
+  const numBookings = bookings.length;
 
   // stat2
+  const sales = bookings.reduce((acc, current) => acc + current.totalPrice, 0);
 
   return (
     <>
-      <Stat icon={<HiOutlineBriefcase />} title="Bookings" color="blue" value={numBookings} />
+      <Stat
+        icon={<HiOutlineBriefcase />}
+        title="Bookings"
+        color="blue"
+        value={numBookings}
+      />
 
-      <Stat icon={<HiOutlineBanknotes />} title="Sales" color="green" />
+      <Stat
+        icon={<HiOutlineBanknotes />}
+        title="Sales"
+        color="green"
+        value={formatCurrency(sales)}
+      />
 
       <Stat icon={<HiOutlineCalendarDays />} title="Check ins" color="indigo" />
 
@@ -24,4 +41,4 @@ function Statistics({ bookings, confirmedStays }) {
   );
 }
 
-export default Statistics
+export default Statistics;
