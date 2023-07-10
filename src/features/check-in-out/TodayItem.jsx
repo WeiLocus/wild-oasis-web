0435c30bed2c10de100a1f995c423e0f65c71427
WeiLocus <marsuo52@gmail.com>
2023-07-10 22:51:11 +0800

    feat: render data in TodayItem component

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,9 +1,41 @@
-function TodayItem({data}) {
-    const { id, status, guests, numNights } = data;
+import styled from "styled-components";
+import Tag from "../../ui/Tag";
+import Flag from "../../ui/Flag";
+import Button from "../../ui/Button";
+
+const StyledTodayItem = styled.li`
+  display: grid;
+  grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
+  gap: 1.2rem;
+  align-items: center;
+  padding: 0.8rem 0;
+  border-bottom: 1px solid var(--color-grey-100);
+  font-size: 1.4rem;
+`;
+
+const Guest = styled.div`
+  font-weight: 500;
+`
+
+function TodayItem({ data }) {
+  const {
+    id,
+    status,
+    guests: { fullName, countryFlag, country },
+    numNights,
+  } = data;
   return (
-    <div>
-    </div>
-  )
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
+      {status === "checked-in" && <Tag type="blue">Departing</Tag>}
+
+      <Flag src={countryFlag} alt={`Flag of ${country}`} />
+      <Guest>{fullName}</Guest>
+      <div>{numNights} nights</div>
+
+      {status === "unconfirmed" && <Button size="small" variation="primary">Check in</Button>}
+    </StyledTodayItem>
+  );
 }
 
-export default TodayItem
+export default TodayItem;
