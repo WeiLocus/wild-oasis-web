9934f29e0d700d01446b9c1dd011a819c5ac34c7
WeiLocus <marsuo52@gmail.com>
2026-04-04 21:13:52 +0800

    fix: "移除未使用變數，修正env拿取資料"

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -1,21 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBookingDetail } from "../../services/apiBookings";
-import { useParams } from "react-router-dom";
-import supabase from "../../services/supabase";
+import { useQuery } from '@tanstack/react-query'
+import { getBookingDetail } from '../../services/apiBookings'
+import { useParams } from 'react-router-dom'
 
 export function useBooking() {
-  const { bookingId } = useParams();
+  const { bookingId } = useParams()
 
   const {
     isLoading,
     data: booking,
     error,
   } = useQuery({
-    queryKey: ["booking", bookingId],
+    queryKey: ['booking', bookingId],
     queryFn: () => getBookingDetail(bookingId),
     // react query try to fetch data three times by default
     retry: false,
-  });
+  })
 
-  return { isLoading, booking, error };
+  return { isLoading, booking, error }
 }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,32 +1,30 @@
-import { formatDistance, parseISO } from "date-fns";
-import { differenceInDays } from "date-fns/esm";
+import { formatDistance, parseISO } from 'date-fns'
 
 export const formatCurrency = (value) =>
-  new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(
-    value
-  );
+  new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
+    value,
+  )
 
 // Return the distance between the given dates in words
 export const formatDistanceFromNow = (dateStr) => {
   const date = formatDistance(parseISO(dateStr), new Date(), {
     addSuffix: true,
   })
-    .replace("about ", "")
-    .replace("in", "In");
-  return date;
-};
+    .replace('about ', '')
+    .replace('in', 'In')
+  return date
+}
 
-
- // spuabase needs an ISO date string ,string will be different on every render, we use this trick to remove any time
+// spuabase needs an ISO date string ,string will be different on every render, we use this trick to remove any time
 // 要與supabase 的creates_at 做準確的比較
-export const getToday = function(options = {}) {
-  const today = new Date();
+export const getToday = function (options = {}) {
+  const today = new Date()
   // options.end true，將日期設置為當天的最後一秒
   if (options?.end) {
     // fix the date to the current day
-    today.setUTCHours(23, 59, 59, 999);
+    today.setUTCHours(23, 59, 59, 999)
   } else {
-    today.setUTCHours(0,0,0,0)
+    today.setUTCHours(0, 0, 0, 0)
   }
   return today.toISOString()
 }
